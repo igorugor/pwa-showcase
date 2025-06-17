764af8b3e19190771d758f50cb4db97c61253105
igorugor <igor18052003@gmail.com>
2025-06-17 23:46:04 +0300

    feat: set up pwa configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA, type ManifestOptions } from 'vite-plugin-pwa';
+
+const manifest: Partial<ManifestOptions> = {
+	theme_color: '#8936FF',
+	background_color: '#2EC6FE',
+	icons: [
+		{ purpose: 'maskable', sizes: '512x512', src: 'icon512_maskable.png', type: 'image/png' },
+		{ purpose: 'any', sizes: '512x512', src: 'icon512_rounded.png', type: 'image/png' },
+	],
+	orientation: 'any',
+	display: 'standalone',
+	lang: 'ru-RU',
+	name: 'PWA-showcase',
+	short_name: 'PWASC',
+	start_url: '/',
+};
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [
+		react(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			workbox: {
+				globPatterns: ['**/*.{html,js,css,svg,png,ico}'],
+			},
+			manifest,
+			devOptions: {
+				enabled: true,
+			},
+		}),
+	],
+});
