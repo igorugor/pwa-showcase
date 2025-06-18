bc2ba0e054b1eed7b6db23aaeabc0a5813fe8dab
igorugor <igor18052003@gmail.com>
2025-06-18 23:35:41 +0300

    updated colors

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { VitePWA, type ManifestOptions } from 'vite-plugin-pwa';
 
 const manifest: Partial<ManifestOptions> = {
-	theme_color: '#8936FF',
+	theme_color: '#242424',
 	background_color: '#242424',
 	icons: [
 		{ purpose: 'maskable', sizes: '512x512', src: 'icon512_maskable.png', type: 'image/png' },
